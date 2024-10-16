3b789a1c7a2466609ef21427c7d530716e616d5c
pervertedpokepuffs <aaadlima97@gmail.com>
2024-10-16 13:51:04 +0800

    Added handling for viewport resize.

--- src/components/nav/navbar-menu.tsx
+++ src/components/nav/navbar-menu.tsx
@@ -1,4 +1,14 @@
-import { Component, createEffect, createMemo, JSX, Show, splitProps } from "solid-js";
+import {
+    Component,
+    createEffect,
+    createMemo,
+    createSignal,
+    JSX,
+    onCleanup,
+    onMount,
+    Show,
+    splitProps,
+} from "solid-js";
 import { twJoin } from "tailwind-merge";
 import { solidTwMerge } from "../../utils/solidTwMerge";
 import { useNavbar } from "./navbar-context";
@@ -13,37 +23,47 @@ export const Menu: Component<MenuProps> = (props) => {
     );
 
     const { topNavbarRef, isMenuOpen } = useNavbar();
+    const [menuHeight, setMenuHeight] = createSignal(0);
 
     const getMenuHeight = () => {
         const navbarBottom =
-            topNavbarRef()?.getBoundingClientRect().bottom || 0;
+        topNavbarRef()?.getBoundingClientRect().bottom || 0;
         const windowHeight = document.documentElement.clientHeight;
         return windowHeight - navbarBottom;
     };
 
-    const classStr = createMemo(() => solidTwMerge(
-        styling,
-        twJoin(
-            "backdrop-blur-sm",
-            "w-10/12 sm:w-96",
-            "absolute",
-            "transform",
-            "transition-transform",
-            "overflow-y-hidden hover:overflow-y-auto",
-            "overflow-x-hidden"
-        ),
-        isMenuOpen() && "translate-x-0",
-        !isMenuOpen() && "-translate-x-full",
-    ));
+    const updateMenuHeight = () => setMenuHeight(getMenuHeight());
+    
+    onMount(() => {
+        updateMenuHeight();
+        window.addEventListener("resize", updateMenuHeight);
+    })
 
-    createEffect(() => console.log(isMenuOpen(), classStr()));
+    onCleanup(() => window.removeEventListener("resize", updateMenuHeight));
+
+    const classStr = createMemo(() =>
+        solidTwMerge(
+            styling,
+            twJoin(
+                "backdrop-blur-sm",
+                "w-10/12 sm:w-96",
+                "absolute",
+                "transform",
+                "transition-transform",
+                "overflow-y-hidden hover:overflow-y-auto",
+                "overflow-x-hidden",
+            ),
+            isMenuOpen() && "translate-x-0",
+            !isMenuOpen() && "-translate-x-full",
+        ),
+    );
 
     return (
         <div
             {...props}
             class={classStr()}
             style={{
-                height: `${getMenuHeight()}px`,
+                height: `${menuHeight()}px`,
             }}
         >
             {local.children}
