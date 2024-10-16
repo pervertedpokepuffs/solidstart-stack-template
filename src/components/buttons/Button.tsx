1f3a79dd04414ed76d96d50950719af4df0ebf11
pervertedpokepuffs <aaadlima97@gmail.com>
2024-10-16 10:30:13 +0800

    Updated button to use common styling generator.

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,7 @@
-import clsx from "clsx";
 import { Component, JSX, splitProps } from "solid-js";
+import { twJoin } from "tailwind-merge";
 import { solidTwMerge } from "../../utils/solidTwMerge";
+import buttonCommonStyling from "./button-common-styling";
 
 export type ButtonProps = {
     children: JSX.Element;
@@ -16,23 +17,7 @@ export const Button: Component<ButtonProps> = (props) => {
 
     const classStr = solidTwMerge(
         styling,
-        clsx(
-            [
-                "transition-color",
-                "duration-200",
-                "p-4",
-                "border",
-                "border-slate-200",
-                "hover:border-slate-500",
-                "rounded-2xl",
-            ],
-            {
-                "bg-slate-50 dark:text-white dark:bg-slate-500 text-current hover:bg-slate-200 active:bg-slate-300 dark:hover:bg-slate-400 dark:active:bg-slate-300 focus:ouline-none focus:ring focus:ring-slate-200": !local?.buttonColor,
-                "bg-primary text-white hover:bg-primary-600 active:bg-primary-700 dark:hover:bg-primary-400 dark:active:bg-primary-300 focus:outline-none focus:ring focus:ring-primary-300": local?.buttonColor == "primary",
-                "bg-secondary text-white hover:bg-secondary-600 active:bg-secondary-700 dark:hover:bg-secondary-400 dark:active:bg-secondary-300 focus:outline-none focus:ring focus:ring-secondary-300": local?.buttonColor == "secondary",
-                "bg-tertiary-500 text-white hover:bg-tertiary-600 active:bg-tertiary-700 dark:hover:bg-tertiary-400 dark:active:bg-tertiary-300 focus:outline-none focus:ring focus:ring-tertiary-300": local?.buttonColor == "tertiary",
-            },
-        ),
+        twJoin("rounded-2xl", buttonCommonStyling(local.buttonColor)),
     );
 
     return (
---
+++ src/components/buttons/button-common-styling.ts
@@ -0,0 +1,22 @@
+import { twMerge } from "tailwind-merge";
+
+export default (type?: "primary" | "secondary" | "tertiary") =>
+    twMerge(
+        "transition-color",
+        "duration-200",
+        "p-4",
+        "border",
+        "border-slate-200",
+        "hover:border-slate-500",
+        "bg-slate-50 dark:bg-slate-500",
+        "text-current dark:text-slate-200",
+        "hover:bg-slate-200 dark:hover:bg-slate-400",
+        "active:bg-slate-300 dark:active:bg-slate-300",
+        "focus:outline-none focus:ring focus:ring-slate-200",
+        type == "primary" &&
+            "bg-primary dark:bg-primary text-slate-200 hover:bg-primary-600 active:bg-primary-700 dark:hover:bg-primary-400 dark:active:bg-primary-300 focus:outline-none focus:ring focus:ring-primary-300",
+        type == "secondary" &&
+            "bg-secondary dark:bg-secondary text-slate-200 hover:bg-secondary-600 active:bg-secondary-700 dark:hover:bg-secondary-400 dark:active:bg-secondary-300 focus:outline-none focus:ring focus:ring-secondary-300",
+        type == "tertiary" &&
+            "bg-tertiary dark:bg-tertiary text-slate-200 hover:bg-tertiary-600 active:bg-tertiary-700 dark:hover:bg-tertiary-400 dark:active:bg-tertiary-300 focus:outline-none focus:ring focus:ring-tertiary-300",
+    );
